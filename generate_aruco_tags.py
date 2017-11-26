535dbcb421b3c3394114b6f44dca2db0c6f65c88
Peter F. Klemperer <peter@peterklemperer.com>
2017-11-26 18:37:41 -0500

    generated more tags for the two new tag boards

--- generate_aruco_tags.py
+++ generate_aruco_tags.py
@@ -3,7 +3,7 @@ import cv2.aruco as aruco
 
 marker_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 
-for i in range( 0, 20 ):
+for i in range( 21, 41 ):
     marker_img = marker_dict.drawMarker(i, 100)
     filename_str = "ARUCO_DICT_6X6_250/ARUCO_DICT_6X6_250_ID_" + str(i) + ".jpg"
     cv2.imwrite( filename_str, marker_img )
